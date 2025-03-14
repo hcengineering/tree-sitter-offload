9fc90b72efa45c294eb94d5c8207bfc55a573362
Nikolay Marchuk <nikolay.marchuk@hardcoreeng.com>
2025-03-14 23:33:58 +0700

    Support multi-language ranges, combined folds

--- src/ranges.rs
+++ src/ranges.rs
@@ -1,8 +1,9 @@
-use std::ops::Range;
+use std::{char, collections::HashMap, ops::Range, sync::Arc, usize};
 
 use jni::{
     errors::Result as JNIResult,
     objects::{AutoLocal, JCharArray, JClass, JMethodID, JObject, JObjectArray, JValue},
+    strings::JNIString,
     sys::{jboolean, jint, jsize},
     JNIEnv,
 };
@@ -14,7 +15,8 @@ use crate::{
     language_registry::with_language,
     predicates::AdditionalPredicates,
     query::RecodingUtf16TextProvider,
-    syntax_snapshot::{SyntaxSnapshot, SyntaxSnapshotDesc},
+    syntax_snapshot::{SyntaxSnapshot, SyntaxSnapshotDesc, SyntaxSnapshotEntryContent},
+    Language, LanguageId,
 };
 use once_cell::sync::OnceCell as JOnceLock;
 
@@ -74,75 +76,117 @@ impl RangesQuery {
 
 fn collect_ranges(
     snapshot: &SyntaxSnapshot,
-    query: &RangesQuery,
+    query_selector: impl Fn(&Language) -> Option<Arc<RangesQuery>>,
+    query_cache: &mut HashMap<LanguageId, Arc<RangesQuery>>,
     text: &[u16],
     byte_range: Range<usize>,
-    mut use_inner: bool,
-) -> Vec<(usize, tree_sitter::Range)> {
+    use_inner: bool,
+) -> Vec<((LanguageId, usize), tree_sitter::Range, usize)> {
     let mut ranges = Vec::new();
-    let mut cursor = QueryCursor::new();
-    cursor.set_byte_range(byte_range);
     let text_provider = RecodingUtf16TextProvider::new(text);
-    let tree = snapshot.main_tree();
-    let mut matches = cursor.matches(&query.query, tree.root_node(), &text_provider);
-    while let Some(query_match) = matches.next() {
-        if !query
-            .predicates
-            .satisfies_predicates(&mut &text_provider, query_match)
-        {
+    for entry in &snapshot.entries {
+        if byte_range.start >= entry.byte_range.end || byte_range.end <= entry.byte_range.start {
             continue;
         }
-        let mut start_byte: Option<usize> = None;
-        let mut end_byte: Option<usize> = None;
-        let mut start_point: Option<tree_sitter::Point> = None;
-        let mut end_point: Option<tree_sitter::Point> = None;
-        let nodes = query_match.nodes_for_capture_index(query.main_capture_id);
-        for node in nodes {
-            if start_byte.is_none_or(|b| node.start_byte() < b) {
-                start_byte = Some(node.start_byte());
-                start_point = Some(node.start_position());
+        let SyntaxSnapshotEntryContent::Parsed { language, tree } = &entry.content else {
+            continue;
+        };
+        let query = if let Some(query) = query_cache.get(language) {
+            query
+        } else {
+            let Ok(Some(query)) = with_language(*language, |language| query_selector(language))
+            else {
+                continue;
+            };
+            query_cache.entry(*language).or_insert(query)
+        };
+        let mut cursor = QueryCursor::new();
+        cursor.set_byte_range(entry.byte_range.clone());
+        let mut matches = cursor.matches(
+            &query.query,
+            tree.root_node_with_offset(entry.byte_offset, entry.point_offset),
+            &text_provider,
+        );
+        while let Some(query_match) = matches.next() {
+            if !query
+                .predicates
+                .satisfies_predicates(&mut &text_provider, query_match)
+            {
+                continue;
             }
-            if end_byte.is_none_or(|b| node.end_byte() > b) {
-                end_byte = Some(node.end_byte());
-                end_point = Some(node.end_position());
-            }
-        }
-        use_inner |= query
-            .query
-            .property_settings(query_match.pattern_index)
-            .iter()
-            .any(|p| p.key.as_ref() == "range.inner");
-        for capture in query_match.captures {
-            if Some(capture.index) == query.start_capture_id {
-                if use_inner {
-                    start_byte = Some(capture.node.end_byte());
-                    start_point = Some(capture.node.end_position());
-                } else {
-                    start_byte = Some(capture.node.start_byte());
-                    start_point = Some(capture.node.start_position());
+            let mut start_byte: Option<usize> = None;
+            let mut end_byte: Option<usize> = None;
+            let mut next_byte: Option<usize> = None;
+            let mut start_point: Option<tree_sitter::Point> = None;
+            let mut end_point: Option<tree_sitter::Point> = None;
+            let nodes = query_match.nodes_for_capture_index(query.main_capture_id);
+            for node in nodes {
+                if start_byte.is_none_or(|b| node.start_byte() < b) {
+                    start_byte = Some(node.start_byte());
+                    start_point = Some(node.start_position());
                 }
-            } else if Some(capture.index) == query.end_capture_id {
-                if use_inner {
-                    end_byte = Some(capture.node.start_byte());
-                    end_point = Some(capture.node.start_position());
+                if end_byte.is_none_or(|b| node.end_byte() > b) {
+                    end_byte = Some(node.end_byte());
+                    end_point = Some(node.end_position());
+                }
+                if let Some(next_node) = node.next_sibling() {
+                    if next_byte.is_none_or(|b| next_node.start_byte() > b) {
+                        next_byte = Some(next_node.start_byte())
+                    }
                 } else {
-                    end_byte = Some(capture.node.end_byte());
-                    end_point = Some(capture.node.end_position());
+                    next_byte = Some(node.end_byte())
                 }
             }
-        }
-        if let (Some(start_byte), Some(end_byte), Some(start_point), Some(end_point)) =
-            (start_byte, end_byte, start_point, end_point)
-        {
-            ranges.push((
-                query_match.pattern_index,
-                tree_sitter::Range {
-                    start_byte,
-                    end_byte,
-                    start_point,
-                    end_point,
-                },
-            ));
+            let use_inner = use_inner
+                || query
+                    .query
+                    .property_settings(query_match.pattern_index)
+                    .iter()
+                    .any(|p| p.key.as_ref() == "range.inner");
+            for capture in query_match.captures {
+                if Some(capture.index) == query.start_capture_id {
+                    if use_inner {
+                        start_byte = Some(capture.node.end_byte());
+                        start_point = Some(capture.node.end_position());
+                    } else {
+                        start_byte = Some(capture.node.start_byte());
+                        start_point = Some(capture.node.start_position());
+                    }
+                } else if Some(capture.index) == query.end_capture_id {
+                    if use_inner {
+                        end_byte = Some(capture.node.start_byte());
+                        end_point = Some(capture.node.start_position());
+                        next_byte = Some(capture.node.start_byte());
+                    } else {
+                        end_byte = Some(capture.node.end_byte());
+                        end_point = Some(capture.node.end_position());
+                        if let Some(next_node) = capture.node.next_sibling() {
+                            next_byte = Some(next_node.start_byte())
+                        } else {
+                            next_byte = Some(capture.node.end_byte())
+                        }
+                    }
+                }
+            }
+            if let (
+                Some(start_byte),
+                Some(end_byte),
+                Some(start_point),
+                Some(end_point),
+                Some(next_byte),
+            ) = (start_byte, end_byte, start_point, end_point, next_byte)
+            {
+                ranges.push((
+                    (*language, query_match.pattern_index),
+                    tree_sitter::Range {
+                        start_byte,
+                        end_byte,
+                        start_point,
+                        end_point,
+                    },
+                    next_byte,
+                ));
+            }
         }
     }
     ranges
@@ -170,22 +214,16 @@ pub extern "system" fn Java_com_hulylabs_treesitter_rusty_TreeSitterNativeRanges
     ) -> JNIResult<JObjectArray<'local>> {
         let snapshot = SyntaxSnapshotDesc::from_java_object(env, snapshot)?;
         let range_desc = RangeDesc::new(env)?;
-        let Some(query) = with_language(snapshot.base_language(), |language| {
-            language.parser_info().indents_query.clone()
-        })
-        .ok()
-        .flatten() else {
-            let obj_array = env.new_object_array(0, &range_desc.class, JObject::null())?;
-            return Ok(obj_array);
-        };
         let text_length = env.get_array_length(&text)?;
         let mut text_buffer = vec![0u16; text_length as usize];
         env.get_char_array_region(&text, 0, &mut text_buffer)?;
 
         let use_inner = use_inner != 0;
+        let mut query_cache = HashMap::new();
         let ranges = collect_ranges(
             snapshot,
-            &query,
+            |l| l.parser_info().indents_query.clone(),
+            &mut query_cache,
             &text_buffer,
             ((start_offset * 2) as usize)..((end_offset * 2) as usize),
             use_inner,
@@ -193,7 +231,7 @@ pub extern "system" fn Java_com_hulylabs_treesitter_rusty_TreeSitterNativeRanges
 
         let ranges_array =
             env.new_object_array(ranges.len() as jsize, &range_desc.class, JObject::null())?;
-        for (index, (_, range)) in ranges.into_iter().enumerate() {
+        for (index, (_, range, _)) in ranges.into_iter().enumerate() {
             let range_obj = range_desc.to_java_object(env, range)?;
             let range_obj = env.auto_local(range_obj);
             env.set_object_array_element(&ranges_array, index as i32, range_obj)?;
@@ -242,7 +280,7 @@ impl<'local> FoldRangeDesc<'local> {
         &self,
         env: &mut JNIEnv<'local>,
         range: tree_sitter::Range,
-        collapsed_text: Option<&str>,
+        collapsed_text: Option<impl Into<JNIString>>,
         collapsed_by_default: bool,
     ) -> JNIResult<JObject<'local>> {
         let range_obj = self.range_desc.to_java_object(env, range)?;
@@ -289,33 +327,27 @@ pub extern "system" fn Java_com_hulylabs_treesitter_rusty_TreeSitterNativeRanges
     ) -> JNIResult<JObjectArray<'local>> {
         let snapshot = SyntaxSnapshotDesc::from_java_object(env, snapshot)?;
         let fold_range_desc = FoldRangeDesc::new(env)?;
-        let Some(query) = with_language(snapshot.base_language(), |language| {
-            language.parser_info().folds_query.clone()
-        })
-        .ok()
-        .flatten() else {
-            let obj_array = env.new_object_array(0, &fold_range_desc.class, JObject::null())?;
-            return Ok(obj_array);
-        };
         let text_length = env.get_array_length(&text)?;
         let mut text_buffer = vec![0u16; text_length as usize];
         env.get_char_array_region(&text, 0, &mut text_buffer)?;
 
         let use_inner = use_inner != 0;
+        let mut query_cache = HashMap::new();
         let ranges = collect_ranges(
             snapshot,
-            &query,
+            |l| l.parser_info().folds_query.clone(),
+            &mut query_cache,
             &text_buffer,
             ((start_offset * 2) as usize)..((end_offset * 2) as usize),
             use_inner,
         );
-
-        let ranges_array = env.new_object_array(
-            ranges.len() as jsize,
-            &fold_range_desc.class,
-            JObject::null(),
-        )?;
-        for (index, (pattern_id, range)) in ranges.into_iter().enumerate() {
+        let mut combined_ranges: Vec<(usize, tree_sitter::Range, bool, Option<&str>, usize)> =
+            Vec::new();
+        let mut last_combined_idx: HashMap<usize, usize> = HashMap::new();
+        'outer: for ((language_id, pattern_id), range, next_byte) in ranges {
+            let query = query_cache
+                .get(&language_id)
+                .expect("query exists in cache if returned from collect_ranges");
             let mut collapsed_text = None;
             let mut collapsed_by_default = false;
             let properties = query.query.property_settings(pattern_id as usize);
@@ -326,12 +358,67 @@ pub extern "system" fn Java_com_hulylabs_treesitter_rusty_TreeSitterNativeRanges
                 if property.key.as_ref() == "fold.collapsed" {
                     collapsed_by_default = true;
                 }
+                if property.key.as_ref() == "fold.combined-lines" {
+                    if let Some((_, last_range, _, _, last_next_byte)) = last_combined_idx
+                        .get(&pattern_id)
+                        .and_then(|idx| combined_ranges.get_mut(*idx))
+                    {
+                        if *last_next_byte == range.start_byte
+                            && range.start_point.column == last_range.start_point.column
+                            && (last_range.end_point.row + 1 == range.start_point.row
+                                || last_range.end_point.row == range.start_point.row)
+                        {
+                            last_range.end_byte = range.end_byte;
+                            last_range.end_point = range.end_point;
+                            *last_next_byte = next_byte;
+                            continue 'outer;
+                        }
+                    }
+                    last_combined_idx.insert(pattern_id, combined_ranges.len());
+                }
+            }
+            combined_ranges.push((
+                pattern_id,
+                range,
+                collapsed_by_default,
+                collapsed_text,
+                next_byte,
+            ));
+        }
+        let ranges_array = env.new_object_array(
+            combined_ranges.len() as jsize,
+            &fold_range_desc.class,
+            JObject::null(),
+        )?;
+        for (index, (_, mut range, collapsed_by_default, collapsed_text, _)) in
+            combined_ranges.into_iter().enumerate()
+        {
+            // Some nodes may include newline at the end, but folds should not end with newline
+            if text_buffer[range.end_byte / 2 - 1] == '\n' as u16 {
+                range.end_byte -= 1;
+                range.end_point.row -= 1;
+                let line_end_offset = range.end_byte / 2 - 1;
+                let mut offset = line_end_offset;
+                let line_start_offset = loop {
+                    let new_offset = offset.saturating_sub(1);
+                    if text_buffer[new_offset] == ('\n' as u16) || new_offset == 0 {
+                        break offset;
+                    }
+                    offset = new_offset;
+                };
+                range.end_point.column = char::decode_utf16(
+                    text_buffer[line_start_offset..line_start_offset]
+                        .iter()
+                        .copied(),
+                )
+                .count();
             }
             let obj =
                 fold_range_desc.to_java_object(env, range, collapsed_text, collapsed_by_default)?;
             let obj = env.auto_local(obj);
             env.set_object_array_element(&ranges_array, index as i32, obj)?;
         }
+
         Ok(ranges_array)
     }
     let result = inner(
